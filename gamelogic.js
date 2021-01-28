bcd3c275168174dc8eb1a6c957dc9ce89955b1ba
jodelajo <jodelajo@gmail.com>
2021-01-28 18:06:21 +0100

    created loop

--- gamelogic.js
+++ gamelogic.js
@@ -1,9 +1,22 @@
 function displayWordSoFar(word, guesses) {
-  // - [ ] zet je testje op test.only (later weer weghalen)
+  // - [x] zet je testje op test.only (later weer weghalen)
   // - [ ] loggen: wat is word, wat is guesses
+  // console.log(word);
+  // console.log(guesses);
+
   // - [ ] javascript, ['a', 'b'] -> "_ _ _ _ _ _ _ _ _ _ "
   // - [ ] javascript "splitten" split() -> ['j','a','v','a','s','c','r','i','p','t']
+  const letterArray = word.split("");
+  console.log(letterArray);
+
   // - [ ] een loop maken -> ['j','a','v','a','s','c','r','i','p','t']
+  let outcome = " ";
+  for (i = 0; i < letterArray.length; i++) {
+    const lettersInWord = letterArray[i];
+    console.log(lettersInWord);
+
+
+  }
   // - [ ] checken: zit de letter in guesses?
   // - [ ] if else
   // - [ ] zo nee: "_ "
